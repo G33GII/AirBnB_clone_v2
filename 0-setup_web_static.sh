cfd8eef8e5665638470d71d239a6968cd65e40ea
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-07 16:49:03 +0100

    update tp compressing static_web

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,19 +5,17 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/{releases/test/,shared/}
+
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+sudo echo "
+<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
