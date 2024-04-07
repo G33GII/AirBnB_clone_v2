e7abb740783519360bd10c16633d3873765bc94b
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-07 17:00:16 +0100

    adding comments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
+# Script that sets up web servers for the deployment of web_static
 
+# Update package lists
 sudo apt-get update
+
+# Install nginx
 sudo apt-get -y install nginx
+
+# Allow Nginx through the firewall
 sudo ufw allow 'Nginx HTTP'
 
+# Create necessary directories
 sudo mkdir -p /data/web_static/{releases/test/,shared/}
 
+# Create a simple HTML file for testing
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "
 <html>
@@ -17,10 +24,14 @@ sudo echo "
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
 
+# Create symbolic link
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
+# Change ownership of /data folder
 sudo chown -R ubuntu:ubuntu /data/
 
+# Update Nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
+# Restart Nginx to apply changes
 sudo service nginx restart
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
- Fabric script that generates a .tgz archive from the contents of
- the web_static folder of your AirBnB Clone repo, using the function do_pack.
+Fabric script that generates a .tgz archive from the contents of
+the web_static folder of your AirBnB Clone repo, using the function do_pack.
 """
 
 from datetime import datetime
@@ -10,13 +10,22 @@ from fabric.api import *
 
 def do_pack():
     """
-    making an archive on web_static folder
+    Create a .tgz archive of the web_static folder.
     """
 
+    # Get the current date and time
     time = datetime.now()
+
+    # Construct the archive filename
     archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+
+    # Create a 'versions' directory if it doesn't exist
     local('mkdir -p versions')
+
+    # Create the .tgz archive
     create = local('tar -cvzf versions/{} web_static'.format(archive))
+
+    # Return the archive filename if successful, otherwise return None
     if create is not None:
         return archive
     else:
