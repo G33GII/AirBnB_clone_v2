dab744b9185b29f6ecdcae04ce34c91a04a4e820
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-21 17:08:38 +0100

    adding more routes

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Flask Script"""
+
+
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route that displays "Hello HBNB!"
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+# Define a route that displays "HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+# Define a route that displays "C " followed by the value of the text variable
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    # Replace underscore symbols (_) with a space
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
+# Define a route that displays "Python " followed by the value of the text variable
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)  # Default value is "is cool"
+def python(text='is_cool'):
+    # Replace underscore symbols (_) with a space
+    text = text.replace('_', ' ')
+    return 'Python ' + text
+
+
+# Run the Flask application on 0.0.0.0, port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
