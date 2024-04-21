8ac306a0f1240e58fa52af899ddda357e9d7f028
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-21 19:35:57 +0100

    update

--- models/state.py
+++ models/state.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""python script"""
-
-
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class State(BaseModel, Base):
+    """ State class """
     __tablename__ = 'states'
-
     name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            from models import storage
             city_objs = []
-            for city in storage.all('City').values():
+            for city in storage.all("City").values():
                 if city.state_id == self.id:
                     city_objs.append(city)
             return city_objs
+    else:
+        cities = relationship("City", back_populates="state",
+                              cascade="all, delete, delete-orphan")
