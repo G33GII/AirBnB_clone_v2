78f8d38f0af115360a0737854e1dbdc6fdd1a579
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-04-21 16:30:43 +0100

    added second route on flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Flask script"""
+
+
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route that displays "Hello HBNB!"
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+# Define a route that displays "HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+# Run the Flask application on 0.0.0.0, port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
